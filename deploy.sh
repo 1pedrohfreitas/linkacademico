09828c65242ffe40c8760d3a5160d62cb9ac03f8
Pedro Henrique Freitas <1pedrohfreitas@gmail.com>
2022-05-31 12:08:42 -0300

    Subindo deploy

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Parando os serviços"
+docker stop petdiniz-linkacademico-1
+
+echo "Removendo os containers"
+docker rm petdiniz-linkacademico-1
+
+echo "Removendo as imagens"
+docker rmi pedrohfreitas/linkacademico:1.0
+
+echo "Construindo a imagem do front"
+docker build -t pedrohfreitas/linkacademico:1.0 .
+
+echo "Subindo a stack de serviços"
+docker compose up -d
